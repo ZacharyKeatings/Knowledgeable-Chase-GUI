2d00533ddcd9b69de90457549ffa33ec3a2c14e1
Zachary Keatings <74317231+ZacharyKeatings@users.noreply.github.com>
2021-11-11 02:05:00 +0000

    Created die_roll without any function

--- main.py
+++ main.py
@@ -1,26 +1,67 @@
+import random
 import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
 root.title("Knowledgeable Chase")
-root.geometry('400x300')
+root.geometry("400x300")
 
 # configure the grid
-root.rowconfigure([0, 1, 2, 3, 4], minsize=50, weight=1)
-root.columnconfigure([0, 1, 2], minsize=50, weight=1)
+root.rowconfigure([0, 1, 2, 3, 4], minsize=10, weight=1)
+root.columnconfigure([0, 1, 2], minsize=10, weight=1)
+
+def die_roll():
+    #This shows in the GUI:
+    roll = tk.Tk()
+    roll.title("Die Roll")
+    roll.geometry("400x300")
+    roll.rowconfigure([0,1], minsize=10, weight=1)
+    roll.columnconfigure([0,1,2], minsize=10, weight=1)
+    
+    roll_result = tk.Label(roll, text="Roll result")
+    roll_result.grid(column=1, row=0, sticky="S")
+    roll_again = ttk.Button(roll, text="Roll Again")
+    roll_again.grid(column=1, row=1, sticky="W")
+    main_menu = ttk.Button(roll, text="Main Menu")
+    main_menu.grid(column=1, row=1, sticky="E")
+
+    def roll_result():
+        pass
+
+    #This shows in the console:
+    dice_number = random.choice([1,2,3,4,5,6])
+    answers = ["Yes", "No"]
+    choice = input("Would you like to roll? Yes or No.\n")
+    choice = choice.capitalize()
+    if choice == "No":
+        clear_screen()
+        print("You just returned from the die roll screen.\n")
+        main_menu()
+    elif choice == "Yes":
+        clear_screen()
+        print(f"You rolled: {dice_number}.\n")
+        die_roll()
+    if choice not in answers:
+        clear_screen()
+        print("That is not a correct selection. Please try again.\n")
+        die_roll()
 
 #Main Menu
-main_title = tk.Label(master=root, text="""Trivial Pursuit Assistant Edition\n
+main_title = tk.Label(root, text="""Trivial Pursuit Assistant Edition\n
 ===================\n
 Please choose an option:""")
 main_title.grid(column=1,row=0)
-login_button = ttk.Button(root, text="Add/Edit/Delete Database")
+login_button = ttk.Button(root, text="Add/Edit/Delete Database", width=20)
 login_button.grid(column=1,row=1)
-log_button = ttk.Button(root, text="Get A Question")
+log_button = ttk.Button(root, text="Get A Question", width=20)
 log_button.grid(column=1,row=2)
-log_button = ttk.Button(root, text="Roll A Die")
+log_button = ttk.Button(root, text="Roll A Die", width=20, command=die_roll)
 log_button.grid(column=1,row=3)
-log_button = ttk.Button(root, text="Exit")
+log_button = ttk.Button(root, text="Exit", width=20)
 log_button.grid(column=1,row=4)
+
+
+
+
 
 root.mainloop()
