317217156619076d89a5fbb475b80436a1a63ff2
Zachary Keatings <74317231+ZacharyKeatings@users.noreply.github.com>
2021-11-11 01:36:03 +0000

    Full main menu layout is finished.

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ from tkinter import ttk
 
 root = tk.Tk()
 root.title("Knowledgeable Chase")
-root.geometry('300x200')
+root.geometry('400x300')
 
 # configure the grid
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
-root.columnconfigure(2, weight=1)
+root.rowconfigure([0, 1, 2, 3, 4], minsize=50, weight=1)
+root.columnconfigure([0, 1, 2], minsize=50, weight=1)
 
 #Main Menu
-login_button = ttk.Button(root, text="Add/Edit/Delete Trivia Database")
-login_button.grid(column=1,row=0,pady=5)
+main_title = tk.Label(master=root, text="""Trivial Pursuit Assistant Edition\n
+===================\n
+Please choose an option:""")
+main_title.grid(column=1,row=0)
+login_button = ttk.Button(root, text="Add/Edit/Delete Database")
+login_button.grid(column=1,row=1)
 log_button = ttk.Button(root, text="Get A Question")
-log_button.grid(column=1,row=2,pady=5)
+log_button.grid(column=1,row=2)
 log_button = ttk.Button(root, text="Roll A Die")
-log_button.grid(column=1,row=3,pady=5)
+log_button.grid(column=1,row=3)
 log_button = ttk.Button(root, text="Exit")
-log_button.grid(column=1,row=4,pady=5)
+log_button.grid(column=1,row=4)
 
 root.mainloop()
